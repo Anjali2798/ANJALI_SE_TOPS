fefdcc0555a7e1870270fa3980bab5cc00c1b513
Anjali <anjali.sonejee2704@gmail.com>
2022-12-21 10:56:50 +0530

    21 Dec. 2022

---
+++ c programming/classwork/test 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{ 
+  printf("Fname: Anjali");
+  printf("\nLname: Sonejee");
+  printf("\nAddress: Ahmedabad");
+  printf("\nD.O.B. 27/04/1998");
+  printf("\nc is a \t\tsea");
+  //printf("but it can't see ")
+
+}
