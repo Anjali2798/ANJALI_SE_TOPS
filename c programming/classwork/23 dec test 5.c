3e6311f43845fb97ba392c9a042d8926cadb7b93
Anjali <anjali.sonejee2704@gmail.com>
2022-12-26 08:24:30 +0530

    Create 23 dec test 5.c

---
+++ c programming/classwork/23 dec test 5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main
+{
+	int no = 29;
+	char ch = 'u';
+	float per = 58.98;
+	double stock = 23908033848.7654775787;
+	char name[12] = "top tech";
+	char mobile[12] = 7201995696;
+	
+	printf("%d\n",no);
+	printf("%c\n",ch);
+	printf("%.2f",per);
+	printf("\n%lf",stock);
+	printf("%s\n",name);
+	printf("%s\n",mobile);
+}
