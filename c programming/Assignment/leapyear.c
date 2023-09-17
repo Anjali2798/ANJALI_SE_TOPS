68355ed1782edad3ae22735e57ed5e731b42ee76
Anjali <anjali.sonejee2704@gmail.com>
2023-09-17 15:40:46 +0530

    yes

--- c programming/Assignment/leapyear.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int days;
-    printf("\nEnter days= ");
-    scanf("%d",&days);
-    (days>365)? printf("year is leap year"): printf("year is not a leap year");
-}
