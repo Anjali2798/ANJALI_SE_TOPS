36a3a873ff79c0373ab6bdd17eb95b0e22e051bb
Anjali <anjali.sonejee2704@gmail.com>
2024-04-26 13:48:15 +0530

    Update que 6.2 swap_without_temp.py

--- python/New folder/Module – 2 (Fundamentals of python)/que 6.2 swap_without_temp.py
+++ python/New folder/Module – 2 (Fundamentals of python)/que 6.2 swap_without_temp.py
@@ -1,9 +1,5 @@
 a=int(input("enter the value of a ")) #a=3
 b=int(input("enter the value of b ")) #b=4
-
-c=a+b #c=3+4=7
-a=c-a #a=7-3=4
-b=c-a #b=7-4=3
-
+a,b = b,a
 print("after swaping value of a is",a)
 print("after swaping value of b is",b)
